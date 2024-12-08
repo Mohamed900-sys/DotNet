4dcd23e6520a88e6e1fb33adb42f32d0c366a147
AmineElHablani <aminehableni1898@gmail.com>
2024-12-08 18:54:19 +0100

    services pack and subscription

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FitnessWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessWebApp.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Subscription> Subscriptions { get; set; }
+		public DbSet<Packs> Packs { get; set; }
+
+	}
+}
